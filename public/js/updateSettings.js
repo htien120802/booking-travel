b8c2b19af2ebf900db4339c1d609e2f3d4936d20
vthgn <vanthuan2004@gmail.com>
2023-11-15 00:54:31 +0700

    Update gitignore

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -5,12 +5,15 @@ import { showAlert } from './alerts';
 // type is either 'password' or 'data'
 export const updateSettings = async (data, type) => {
   try {
-    const url = type === 'password' ? '/api/v1/users/updateMyPassword' : '/api/v1/users/updateMe';
+    const url =
+      type === 'password'
+        ? '/api/v1/users/updateMyPassword'
+        : '/api/v1/users/updateMe';
 
     const res = await axios({
       method: 'PATCH',
       url,
-      data
+      data,
     });
 
     if (res.data.status === 'success') {
